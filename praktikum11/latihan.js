c2d800defd7a56b31f65de6e4e0bb791dbf4650c
Nicky30yt <145972059+Nicky30yt@users.noreply.github.com>
2023-12-17 23:21:44 +0700

    Add files via upload
    Nicky Bryan Pradana_0110223260_TI08

---
+++ praktikum11/latihan.js
@@ -0,0 +1,59 @@
+// ini dengan variabel dengan let
+let nama = "nicky bryan pradana"
+console.log(nama)
+
+// ini variabel dengan cost
+const firstname ="nicky"
+const lastname ="bryan"
+console.log(firstname + lastname)
+
+// ini tipe data string
+const gender = "LK";
+console.log(typeof gender)
+
+// ini tipe data int
+const age = 21
+console.log(typeof age)
+
+// ini tipedata boolean
+const ismaried = false;
+console.log(ismaried)
+console.log(typeof ismaried)
+
+// operasi aritmatika
+const x = 9
+const y = 10
+
+// pertambahan
+const z = x + y 
+
+// pengurangan
+const m = x - y
+
+// perkalian 
+const n = x * y
+
+// pembagian 
+const c = 10 % 2
+
+console.log(z)
+console.log(m)
+console.log(n)
+console.log(c)
+
+
+
+// function
+function salam() {
+    alert("assalamualaikum")
+
+}
+salam();
+
+function istrue() {
+    let city = "depok"
+}
+
+console.log(city)
+
+istrue();
